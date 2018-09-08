65bb9d46f38d2c6d535951e4c047503a1a9c7a46
Glenn Edgar <glenn-edgar@onyxengr.com>
2018-09-07 18:28:27 -0700

    move to python3

---
+++ esp32_python_client/libs/esp32_wifi_setup.py
@@ -0,0 +1,17 @@
+
+from .esp32_commands_py3 import ESP32_Message_Generator
+
+
+class ESP32_WIFI_MANAGER(object):
+
+   def __init__(self,serial_handle):
+       self.mess_gen = ESP32_Message_Generator(serial_handle)
+       
+   def write_wifi_setup(self):
+       print("wifi_setup")
+       access_dict = {}
+       access_dict["ssid"] = "onyx_1_G"
+       access_dict["password"] = "read2go"
+       access_dict["hostname"] = "esp32_slave"
+       self.mess_gen.request_write_file("WIFI.MPK",access_dict)
+      
