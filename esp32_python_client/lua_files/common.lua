cb0b3df74c8049d33d03ac2ac5e5600a4395db5f
Glenn Edgar <glenn-edgar@onyxengr.com>
2018-11-02 18:41:39 -0700

    daily commit

--- esp32_python_client/lua_files/common.lua
+++ esp32_python_client/lua_files/common.lua
@@ -1,2 +1,25 @@
 
 print("made it to common setup")
+function table_dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. table_dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
+
+function keys( x)
+  local keyset={}
+  local n=0
+
+  for k,v in pairs(x) do
+       n=n+1
+       keyset[n]=k
+  end
+  return keyset
+  end
